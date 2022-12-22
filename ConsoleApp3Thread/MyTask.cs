09e1d9fbdcd8ef95db1fccdc6927cd8f7a405496
imjays <jpkumar541@gmail.com>
2022-12-22 15:26:17 +0530

    Add project files.

---
+++ ConsoleApp3Thread/MyTask.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3Thread
+{
+    public class Counter
+    {
+        public static int Count;
+    }
+    public class Myclass 
+    {
+        public static void UpdateCount()
+        {
+
+        }
+    }
+    internal class MyTask
+    {
+        public static void TaskOne(object? p)
+        {
+            int num = (int)(p ?? 0);
+            for (int i = 1; i <= num; i++)
+            {
+                Console.WriteLine($"{i}TaskOne.{Thread.CurrentThread.ManagedThreadId}");
+            }         
+        }
+       
+    }
+}
---
+++ ConsoleApp3Thread/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp3Thread;
+
+Thread[] threads = new Thread[10];
+for (int a = 0; a < 10; a++)
+{
+    threads[a] = new Thread(new ThreadStart(Myclass.UpdateCount));
+    threads[a].Start();
+}
+
+for (int a = 0; a < 10; a++)
+{
+       threads[a].Join();
+}
+
+Console.WriteLine($"Thread name: {Thread.CurrentThread.ManagedThreadId}");
+Console.ReadLine();
+
